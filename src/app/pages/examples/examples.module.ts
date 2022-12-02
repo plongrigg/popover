aaf7a6584c6e62d4e2554b5344c76f68532f8626
Peter C.Longrigg <plongrigg@gmail.com>
2022-12-02 16:17:08 -0500

    misc. updates

--- src/app/pages/examples/examples.module.ts
+++ src/app/pages/examples/examples.module.ts
@@ -1,24 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms/forms';
 import { MatInputModule } from '@angular/material/input';
-
 import { MdePopoverModule } from '@fgrid-ngx/mde';
-
 import { ExamplesRoutingModule } from './examples-routing.module';
-
 import { ErrorModule } from '../error/error.module';
-
-import { PageExamplesComponent } from './examples.component';
-import { PageExamplesFormComponent } from './form/form.component';
-import { PageExamplesBuilderComponent } from './builder/builder.component';
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { PageExamplesBuilderComponent } from './builder/builder.component';
+import { PageExamplesComponent } from './examples.component';
+import { PageExamplesFormComponent } from './form/form.component';
 
 
 @NgModule({
